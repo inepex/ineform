80631734ee291e5406d5689b5ed6a7f7c1de2468
tiborsomodi <tibor.somodi@inepex.com>
2011-06-10 14:12:44 +0000

    mistypes in gwt.xml
    git-svn-id: http://ineform.googlecode.com/svn/trunk@91 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/client/async/AsyncStatusIndicator.java
+++ ineframe/src/main/java/com/inepex/ineFrame/client/async/AsyncStatusIndicator.java
@@ -1,10 +1,7 @@
 package com.inepex.ineFrame.client.async;
 
-import com.inepex.inei18n.client.IneFormI18n_old;
 
 public interface AsyncStatusIndicator {
-	public static String DEFAULT_LOADING  = IneFormI18n_old.loading();
-	
 	void onAsyncRequestStarted(String loadingMessage);
 	void onGeneralFailure(String errorMessage);
 	void onForbidden(String forbiddenMessage);
