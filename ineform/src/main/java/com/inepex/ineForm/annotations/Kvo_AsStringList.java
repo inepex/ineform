8e77265d53b413a8b3558f1b7ad0cb67c70bdd38
sebestyencsorba <sebestyen.csorba@inepex.com>
2011-07-19 11:19:13 +0000

    dead code
    git-svn-id: http://ineform.googlecode.com/svn/trunk@167 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineform/src/main/java/com/inepex/ineForm/annotations/Kvo_AsStringList.java
+++
@@ -1,8 +0,0 @@
-package com.inepex.ineForm.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.SOURCE)
-public @interface Kvo_AsStringList {
-}
