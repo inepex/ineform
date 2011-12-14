4e615c2e00d41a6a71070dd84d532ca315cae58a
tiborsomodi <tibor.somodi@inepex.com>
2011-12-14 14:32:20 +0000

    git-svn-id: http://ineform.googlecode.com/svn/trunk@601 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/server/util/ArrayListConcurrentHashMap.java
+++ ineframe/src/main/java/com/inepex/ineFrame/server/util/ArrayListConcurrentHashMap.java
@@ -1,29 +1,30 @@
 package com.inepex.ineFrame.server.util;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ArrayListConcurrentHashMap<T, K> extends ConcurrentHashMap<T, ArrayList<K>>{
+public class ArrayListConcurrentHashMap<T, K> extends ConcurrentHashMap<T, List<K>>{
 	
 	public ArrayListConcurrentHashMap() {
 	}
 	
-	public ArrayList<K> getListById(T id){
+	public List<K> getListById(T id){
 		return get(id);
 	}
 	public boolean isListEmptyById(T id){
 		return ((get(id) == null) || (get(id).size() == 0));
 	}
 	public void addToListById(T id, K element){
-		ArrayList<K> list = get(id);
+		List<K> list = get(id);
 		if(list == null){
 			list = new ArrayList<K>();
 			put(id, list);
 		}
 		list.add(element);
 	}
-	public void addElementListById(T id, ArrayList<K> elementList){
-		ArrayList<K> list = get(id);
+	public void addElementListById(T id, List<K> elementList){
+		List<K> list = get(id);
 		if(list == null){
 			put(id, elementList);
 		}
