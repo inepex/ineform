19e7158955ae2d91c0cc4f856b0499b707927b68
tiborsomodi <tibor.somodi@inepex.com>
2012-01-05 19:02:04 +0000

    prevent cache of nocache.js
    git-svn-id: http://ineform.googlecode.com/svn/trunk@611 7ef67a20-634e-90e6-9ab5-3f2075439470

---
+++ ineframe/src/main/java/com/inepex/ineFrame/server/NoCacheFilter.java
@@ -0,0 +1,34 @@
+package com.inepex.ineFrame.server;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class NoCacheFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setHeader("Last-Modified", new Date().toString());
+        resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+
+        chain.doFilter(request, response);
+    }
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {		
+	}
+
+}
