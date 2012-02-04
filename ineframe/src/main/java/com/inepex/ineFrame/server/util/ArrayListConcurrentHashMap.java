d3cbf550660f10989a0368f6c16680bbaab729d8
istvanszoboszlai <istvan.szoboszlai@inepex.com>
2012-02-04 23:43:48 +0000

    New method for enabling synchronization on an inner list instance.
    git-svn-id: http://ineform.googlecode.com/svn/trunk@634 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/server/util/ArrayListConcurrentHashMap.java
+++ ineframe/src/main/java/com/inepex/ineFrame/server/util/ArrayListConcurrentHashMap.java
@@ -4,23 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * @author balint.steinbach@inepex.com, istvan.szoboszlai@inepex.com
+ *
+ * ConcurrentHashMap of a {@link List} of Items. Ensures creation of the list, and provides convenience functions.
+ *
+ * @param <T> Type of the Key
+ * @param <K> Generic type of the List
+ */
 public class ArrayListConcurrentHashMap<T, K> extends ConcurrentHashMap<T, List<K>>{
 	
-	public ArrayListConcurrentHashMap() {
-	}
+	private static final long serialVersionUID = 1L;
+	
 	
 	public List<K> getListById(T id){
 		return get(id);
 	}
+	
 	public boolean isListEmptyOrNullById(T id){
-		return ((get(id) == null) || (get(id).size() == 0));
+		List<K> list = get(id);
+		return (list == null) || (list.size() == 0);
 	}
-	public boolean isListEmptyById(T id){
-		return (get(id).size() == 0);
-	}
+
 	public boolean isListNullById(T id){
 		return (get(id) == null);
 	}
+	
 	public void addToListById(T id, K element){
 		List<K> list = get(id);
 		if(list == null){
@@ -29,6 +38,24 @@ public class ArrayListConcurrentHashMap<T, K> extends ConcurrentHashMap<T, List<
 		}
 		list.add(element);
 	}
+	
+	/**
+	 * Ensures that a list be created for the given id. The list retoruned by this method
+	 * can be a parameter of a synchronized block.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public List<K> ensureListById(T id) {
+		List<K> list = get(id);
+		if(list == null) {
+			list = new ArrayList<K>(1);
+			put(id, list);
+		}
+		return list;
+		
+	}
+	
 	public void addElementListById(T id, List<K> elementList){
 		List<K> list = get(id);
 		if(list == null){
@@ -38,6 +65,7 @@ public class ArrayListConcurrentHashMap<T, K> extends ConcurrentHashMap<T, List<
 			list.addAll(elementList);
 		}
 	}
+	
 	public void addEmptyListById(T id){
 		put(id, new ArrayList<K>());
 	}
