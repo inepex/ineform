24c2288fce08398fa542d361053dbb2fb0d709f2
stbalint <balint.steinbach@inepex.com>
2012-03-14 14:33:27 +0000

    new basic panel added that implements InePage
    git-svn-id: http://ineform.googlecode.com/svn/trunk@671 7ef67a20-634e-90e6-9ab5-3f2075439470

---
+++ ineframe/src/main/java/com/inepex/ineFrame/client/page/LayoutPanelBasedPage.java
@@ -0,0 +1,42 @@
+package com.inepex.ineFrame.client.page;
+
+import java.util.Map;
+
+import com.google.gwt.user.client.ui.Widget;
+
+import com.inepex.ineFrame.client.misc.HandlerAwareLayoutPanel;
+import com.inepex.ineFrame.client.navigation.InePlace;
+
+public class LayoutPanelBasedPage extends HandlerAwareLayoutPanel implements InePage{
+
+	protected InePlace currentPlace;
+	
+	public LayoutPanelBasedPage() {}
+	
+	@Override
+	public void setCurrentPlace(InePlace place) {
+		this.currentPlace = place;
+	}
+
+	@Override
+	public void setUrlParameters(Map<String, String> urlParams,
+			UrlParamsParsedCallback callback) throws Exception {
+		callback.onUrlParamsParsed();
+	}
+
+	@Override
+	public void onShow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Widget asWidget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public InePlace getCurrentPlace() {
+		return currentPlace;
+	}
+}
