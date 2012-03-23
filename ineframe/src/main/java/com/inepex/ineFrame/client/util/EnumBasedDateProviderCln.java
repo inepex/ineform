ba0585885be48dd0a676f169b1113ae99f8e32fc
sebestyencsorba <sebestyen.csorba@inepex.com>
2012-03-23 12:09:06 +0000

    aviod cookie usage in dateprovider http://code.google.com/p/ineform/issues/detail?id=30
    git-svn-id: http://ineform.googlecode.com/svn/trunk@680 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/client/util/EnumBasedDateProviderCln.java
+++ ineframe/src/main/java/com/inepex/ineFrame/client/util/EnumBasedDateProviderCln.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import com.google.gwt.i18n.client.TimeZone;
 import com.google.gwt.i18n.client.constants.TimeZoneConstants;
-import com.google.gwt.user.client.Cookies;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.inepex.ineFrame.server.util.TimeZoneEnum;
 import com.inepex.ineFrame.shared.util.DateHelper;
@@ -14,7 +12,7 @@ import com.inepex.ineFrame.shared.util.DateProvider;
 
 /**
  * 
- * WARNING! the the time zone was set will be stored in cookie
+ * WARNING! you should initialize before using (and everytime when windows is reloaded)
  * 
  * Based on {@link TimeZoneEnum}'s tzJson field or GWT's {@link TimeZoneConstants}
  * 
@@ -22,36 +20,22 @@ import com.inepex.ineFrame.shared.util.DateProvider;
 @Singleton
 public class EnumBasedDateProviderCln implements DateProvider {
 	
-	public static final String TIMEZONE_COOKIE_ID="EnumBasedDateProviderCln_timezoneJson";
 	private TimeZone tz = null; 
-
-	@Inject
-	EnumBasedDateProviderCln(){
-		String tzJson = Cookies.getCookie(TIMEZONE_COOKIE_ID);
-		if(tzJson!=null) {
-			try {
-				setTimeZoneEnumAndTimeZone(tzJson);
-			} catch (Exception e) {
-				//illegal cookie
-				Cookies.removeCookie(TIMEZONE_COOKIE_ID);
-			}
-		}
-	}
 
 	/**
 	 * 
 	 * @param tzJson - one of {@link TimeZoneEnum}'s tzJson field or GWT's {@link TimeZoneConstants}
 	 */
-	public void setTimeZoneEnumAndTimeZone(String tzJson) {
+	public void setTimeZoneJson(String tzJson) {
 		this.tz=TimeZone.createTimeZone(tzJson);
-		
-		Cookies.setCookie(TIMEZONE_COOKIE_ID,
-				tzJson, DateHelper.addDaysSafe(new Date(), 50));
 	}
 	
 	@Override
 	@SuppressWarnings("deprecation")
 	public Date getDate(Long dateInUTC) {
+		if(tz==null)
+			throw new IllegalStateException("EnumBasedDateProviderCln is not initialized");
+		
 		Date date = new Date(dateInUTC);
 		Long corectedDate =
 				dateInUTC
@@ -64,6 +48,9 @@ public class EnumBasedDateProviderCln implements DateProvider {
 	@Override
 	@SuppressWarnings("deprecation")
 	public Date whatMeansTyped(Long dateInUTC) {
+		if(tz==null)
+			throw new IllegalStateException("EnumBasedDateProviderCln is not initialized");
+		
 		Date date = new Date(dateInUTC);
 		Long corectedDate = 
 			dateInUTC
