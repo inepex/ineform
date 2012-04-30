3498abc7b03db948140785cc69d7c789c919754f
tiborsomodi <tibor.somodi@inepex.com>
2012-04-30 16:31:18 +0000

    use locale in dateformatter
    git-svn-id: http://ineform.googlecode.com/svn/trunk@722 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineform/src/main/java/com/inepex/ineForm/server/util/JavaDateFormatter.java
+++ ineform/src/main/java/com/inepex/ineForm/server/util/JavaDateFormatter.java
@@ -2,19 +2,35 @@ package com.inepex.ineForm.server.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.inepex.ineFrame.shared.util.DateFormatter;
+import com.inepex.inei18n.shared.CurrentLang;
 
 public class JavaDateFormatter implements DateFormatter {
 
-	@Override
-	public String format(String pattern, Date date) {
-		return new SimpleDateFormat(pattern).format(date);
+	private final Provider<CurrentLang> currentLangProv;
+	
+	@Inject
+	public JavaDateFormatter(Provider<CurrentLang> currentLangProv) {
+		super();
+		this.currentLangProv = currentLangProv;
 	}
 
+	private Locale getLocale(){
+		return new Locale(currentLangProv.get().getCurrentLang());
+	}
+	
+	@Override
+	public String format(String pattern, Date date) {
+		return new SimpleDateFormat(pattern, getLocale()).format(date);
+	}
+	
 	@Override
 	public String format(String pattern, Long date) {
-		return new SimpleDateFormat(pattern).format(new Date(date));
+		return new SimpleDateFormat(pattern, getLocale()).format(new Date(date));
 	}
 
 }
