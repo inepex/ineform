19a0f36db9d9a954db27982edf32f3eaaf3f1fa6
stbalint <balint.steinbach@inepex.com>
2012-07-10 10:26:18 +0000

    Custom style can be added to IneTable useing the CellTableResource interfece.
    git-svn-id: http://ineform.googlecode.com/svn/trunk@801 7ef67a20-634e-90e6-9ab5-3f2075439470

---
+++ ineform/src/main/java/com/inepex/ineForm/client/resources/CellTableResources.java
@@ -0,0 +1,10 @@
+package com.inepex.ineForm.client.resources;
+
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.CellTable.Resources;
+
+public interface CellTableResources extends Resources{
+	
+	CellTable.Style cellTableStyle();
+
+}
--- ineform/src/main/java/com/inepex/ineForm/client/resources/ResourceHelper.java
+++ ineform/src/main/java/com/inepex/ineForm/client/resources/ResourceHelper.java
@@ -3,14 +3,13 @@ package com.inepex.ineForm.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.CellTable.Resources;
 import com.google.gwt.user.client.ui.Image;
 
 public class ResourceHelper {
 
 	private static IneResources imageResources = null;
 	private static IneFormResources ifResources = null;
-	private static Resources cellTableResources = null;
+	private static CellTableResources cellTableResources = null;
 	
 	//image helper
 	public static String getImageAsString(ImageResource resource) {
@@ -34,18 +33,18 @@ public class ResourceHelper {
 	
 	
 	//celltable resources
-	public static Resources cellTableResources() {
+	public static CellTableResources cellTableResources() {
 		if(cellTableResources==null)
 			cellTableResources = GWT.create(IneCellTableResources.class);
 		
 		return cellTableResources;
 	}
 	
-	public static void setCellTableResources(Resources cellTableResources) {
+	public static void setCellTableResources(CellTableResources cellTableResources) {
 		ResourceHelper.cellTableResources = cellTableResources;
 	}
 	
-	public interface IneCellTableResources extends Resources {
+	public interface IneCellTableResources extends CellTableResources {
         /**
          * The styles used in this widget.
          */
