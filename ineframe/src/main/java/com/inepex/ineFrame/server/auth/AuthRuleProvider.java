8b17f4407d9644156c64b76632c6fee6a7425ab6
tiborsomodi <tibor.somodi@inepex.com>
2012-09-03 13:28:20 +0000

    javadoc for AuthRuleProvider
    git-svn-id: http://ineform.googlecode.com/svn/trunk@860 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/server/auth/AuthRuleProvider.java
+++ ineframe/src/main/java/com/inepex/ineFrame/server/auth/AuthRuleProvider.java
@@ -5,11 +5,23 @@ import com.inepex.ineom.shared.dispatch.interfaces.ObjectManipulation;
 import com.inepex.ineom.shared.dispatch.interfaces.RelationList;
 
 public interface AuthRuleProvider {
-
+	
+	/**
+	 * Authorize objectManipulation execution. Add rules to AuthRuleSet by AuthRuleSet.ADD()
+	 * Check https://trac.inetrack.com/wiki/PermissionHandling#Logicofauthorizemethod
+	 */
 	public boolean authorize(Long userId, ObjectManipulation objectManipulation, AuthRuleSet authRuleSet);
 	
+	/**
+	 * Authorize objectList execution. Add rules to AuthRuleSet by AuthRuleSet.ADD()
+	 * Check https://trac.inetrack.com/wiki/PermissionHandling#Logicofauthorizemethod
+	 */
 	public boolean authorize(Long userId, ObjectList objectList, AuthRuleSet authRuleSet);
 	
+	/**
+	 * Authorize relationList execution. Add rules to AuthRuleSet by AuthRuleSet.ADD()
+	 * Check https://trac.inetrack.com/wiki/PermissionHandling#Logicofauthorizemethod
+	 */
 	public boolean authorize(Long userId, RelationList relationList, AuthRuleSet authRuleSet);
 	
 }
