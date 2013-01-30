35489fbd97a40af4788a2938a4658c6872e1eb8f
sebestyencsorba <sebestyen.csorba@inepex.com>
2013-01-30 12:58:58 +0000

    font loading
    git-svn-id: http://ineform.googlecode.com/svn/trunk@943 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineform/src/main/java/com/inepex/ineForm/server/tablerenderer/pdf/PdfFontLoader.java
+++ ineform/src/main/java/com/inepex/ineForm/server/tablerenderer/pdf/PdfFontLoader.java
@@ -1,8 +1,11 @@
 package com.inepex.ineForm.server.tablerenderer.pdf;
 
+import java.io.InputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.ByteStreams;
 import com.google.inject.Singleton;
 import com.itextpdf.text.pdf.BaseFont;
 
@@ -10,30 +13,36 @@ import com.itextpdf.text.pdf.BaseFont;
 public class PdfFontLoader {
 
 	private static final Logger _logger = LoggerFactory.getLogger(PdfFontLoader.class);
+	private static final String fontName="FreeSans.ttf";
 	
 	private BaseFont baseFont;
 	private boolean inited=false;
 
 	
 	public BaseFont getBaseFont() {
-		init();
+		mayDoInit();
 		return baseFont;
 	}
 	
-	private void init() {
+	private void mayDoInit() {
 		if(inited)
 			return;
 		
 		inited=true;
-		
-		String fontUrl = getClass().getClassLoader().getResource("FreeSans.ttf").getFile();
-		
-		try {	
-			baseFont = BaseFont.createFont(fontUrl, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+		try(InputStream is = getClass().getClassLoader().getResourceAsStream(fontName)) {
+			baseFont = BaseFont.createFont(
+					fontName,
+					BaseFont.IDENTITY_H,
+					BaseFont.EMBEDDED, 
+					false,
+					ByteStreams.toByteArray(is),
+					new byte[0],
+					false,
+					true);
+			
 			return;
 		} catch (Exception e) {
-			_logger.error("Font not found for pdf export: "+fontUrl, e);
-			_logger.error("Default font is used. May cause problems with unicode characters");
+			_logger.error("Default font is used. May cause problems with unicode characters", e);
 		}
 		
 		try {
