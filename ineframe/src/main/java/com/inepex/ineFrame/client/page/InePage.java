f93365abbc44ee5befa14dcb17ba7d966c8d4aaa
sebestyencsorba <sebestyen.csorba@inepex.com>
2013-04-30 11:52:43 +0000

    asWidget method from interface
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1015 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineframe/src/main/java/com/inepex/ineFrame/client/page/InePage.java
+++ ineframe/src/main/java/com/inepex/ineFrame/client/page/InePage.java
@@ -2,14 +2,13 @@ package com.inepex.ineFrame.client.page;
 
 import java.util.Map;
 
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.inepex.ineFrame.client.navigation.InePlace;
 
-public interface InePage {
+public interface InePage extends IsWidget{
 	public void setCurrentPlace(InePlace place);
 	public void setUrlParameters(Map<String, String> urlParams, UrlParamsParsedCallback callback) throws Exception;
 	public void onShow();
-	public Widget asWidget();
 	
 	public interface UrlParamsParsedCallback {
 		void onUrlParamsParsed();
