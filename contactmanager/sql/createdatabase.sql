2be534a16096410470c17ebbc6e8fe9aa80d350c
sebestyencsorba <sebestyen.csorba@inepex.com>
2013-05-27 14:29:16 +0000

    utf8
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1049 7ef67a20-634e-90e6-9ab5-3f2075439470

--- contactmanager/sql/createdatabase.sql
+++ contactmanager/sql/createdatabase.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE contactmanager;
+CREATE DATABASE contactmanager CHARACTER SET utf8;
 CREATE USER 'manager'@'localhost' IDENTIFIED BY 'manager123';
 GRANT ALL PRIVILEGES ON contactmanager.* TO 'manager'@'localhost';
