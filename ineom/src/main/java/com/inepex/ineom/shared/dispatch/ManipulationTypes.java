e1f261266edd9bff601a15b53a0b718d7883398d
tiborsomodi <tibor.somodi@inepex.com>
2013-06-27 10:49:37 +0000

    added getValuesAsString to ManipulationTypes
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1126 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineom/src/main/java/com/inepex/ineom/shared/dispatch/ManipulationTypes.java
+++ ineom/src/main/java/com/inepex/ineom/shared/dispatch/ManipulationTypes.java
@@ -1,10 +1,20 @@
 package com.inepex.ineom.shared.dispatch;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
+import com.inepex.ineom.shared.IFConsts;
 
 public enum ManipulationTypes implements IsSerializable  {
 		CREATE_OR_EDIT_REQUEST,
 		DELETE,
 		UNDELETE,
-		REFRESH
+		REFRESH;
+		
+		public static String getValuesAsString(){
+			String s = "";
+			for (ManipulationTypes item : values()){
+				s += item.name() + IFConsts.enumValueSplitChar;
+			}
+			if (s.length() > 0) s = s.substring(0, s.length()-1);
+			return s;
+		}
 }
