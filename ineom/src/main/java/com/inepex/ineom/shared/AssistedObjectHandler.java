fe636cf97eb11a71561491cc6a7036918751ce40
tiborsomodi <tibor.somodi@inepex.com>
2013-10-17 12:00:32 +0000

    organize import
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1238 7ef67a20-634e-90e6-9ab5-3f2075439470

--- ineom/src/main/java/com/inepex/ineom/shared/AssistedObjectHandler.java
+++ ineom/src/main/java/com/inepex/ineom/shared/AssistedObjectHandler.java
@@ -5,8 +5,6 @@ import java.util.List;
 import com.inepex.ineom.shared.assistedobject.AssistedObject;
 import com.inepex.ineom.shared.assistedobject.AssistedObjectChecker;
 import com.inepex.ineom.shared.assistedobject.KeyValueObject;
-import com.inepex.ineom.shared.descriptor.fdesc.FDesc;
-import com.inepex.ineom.shared.descriptor.fdesc.PropFDesc;
 import com.inepex.ineom.shared.descriptor.fdesc.RelationFDesc;
 import com.inepex.ineom.shared.descriptorstore.DescriptorStore;
 import com.inepex.ineom.shared.util.SharedUtil;
