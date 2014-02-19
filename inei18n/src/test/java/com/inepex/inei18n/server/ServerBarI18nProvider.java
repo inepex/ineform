755e9b35a99101643c477e60c65367bfc6065b60
tiborsomodi <tibor.somodi@inepex.com>
2014-02-19 14:37:59 +0000

    avoid file change after build
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1316 7ef67a20-634e-90e6-9ab5-3f2075439470

--- inei18n/src/test/java/com/inepex/inei18n/server/ServerBarI18nProvider.java
+++ inei18n/src/test/java/com/inepex/inei18n/server/ServerBarI18nProvider.java
@@ -1,6 +1,9 @@
 package com.inepex.inei18n.server;
-import com.google.inject.Provider;
+import com.inepex.inei18n.server.BarI18n;
+
+import com.inepex.inei18n.server.ServerI18nProvider;
 import com.inepex.inei18n.shared.CurrentLang;
+import com.google.inject.Provider;
 
 public class ServerBarI18nProvider extends ServerI18nProvider<BarI18n> {
 
