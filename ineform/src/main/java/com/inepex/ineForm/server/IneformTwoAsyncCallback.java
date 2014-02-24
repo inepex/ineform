59d27a0020379a082828ea5bd1f9f6da9ac5ac64
tiborsomodi <tibor.somodi@inepex.com>
2014-02-24 17:26:20 +0000

    added IneformTwoAsyncCallback
    git-svn-id: http://ineform.googlecode.com/svn/trunk@1318 7ef67a20-634e-90e6-9ab5-3f2075439470

---
+++ ineform/src/main/java/com/inepex/ineForm/server/IneformTwoAsyncCallback.java
@@ -0,0 +1,44 @@
+package com.inepex.ineForm.server;
+
+public abstract class IneformTwoAsyncCallback <First, Second> {
+
+	private IneformAsyncCallback<First> firstCallback = new IneformAsyncCallback<First>() {
+
+		@Override
+		public void onResponse(First response) {
+			setFirstResponseAndCheckReady(response);
+		}
+	};
+	
+	private IneformAsyncCallback<Second> secondCallback = new IneformAsyncCallback<Second>() {
+
+		@Override
+		public void onResponse(Second response) {
+			setSecondResponseAndCheckReady(response);
+		}
+	};
+	
+	private First firstResponse;
+	private Second secondResponse;
+	
+	private synchronized void setFirstResponseAndCheckReady(First firstResponse){
+		this.firstResponse = firstResponse;
+		if (secondResponse != null) onResponse(this.firstResponse, this.secondResponse);
+	}
+	
+	private synchronized void setSecondResponseAndCheckReady(Second secondResponse){
+		this.secondResponse = secondResponse;
+		if (firstResponse != null) onResponse(this.firstResponse, this.secondResponse);
+	}
+	
+	public IneformAsyncCallback<First> getFirstCallback(){
+		return firstCallback;
+	}
+	
+	public IneformAsyncCallback<Second> getSecondCallback(){
+		return secondCallback;
+	}
+	
+	
+	public abstract void onResponse(First firstResponse, Second secondResponse);
+}
