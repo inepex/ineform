67ff17e304694bc5b9b3fbfaa2e3d3424b922a03
balint.steinbach <balint.steinbach@inepex.com>
2014-10-22 16:36:59 +0200

    add new event

---
+++ ineframe/src/main/java/com/inepex/ineFrame/client/auth/UserLoggedInEvent.java
@@ -0,0 +1,23 @@
+package com.inepex.ineFrame.client.auth;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+public class UserLoggedInEvent extends GwtEvent<UserLoggedInEvent.Handler>{
+
+	public interface Handler extends EventHandler{
+		public void onUserLoggedIn();
+	}
+
+	public static final Type<Handler> TYPE = new Type<Handler>();
+	
+	@Override
+	public Type<Handler> getAssociatedType() {
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(Handler handler) {
+		handler.onUserLoggedIn();
+	}
+}
