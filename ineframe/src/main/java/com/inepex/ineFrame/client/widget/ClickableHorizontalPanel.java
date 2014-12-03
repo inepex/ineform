0f6711c93063ed4a5716c09245b85d7f82ac47d8
tibor.somodi <tibor.somodi@inepex.com>
2014-12-03 15:02:15 +0100

    new: ClickabelHorizontalPanel

---
+++ ineframe/src/main/java/com/inepex/ineFrame/client/widget/ClickableHorizontalPanel.java
@@ -0,0 +1,15 @@
+package com.inepex.ineFrame.client.widget;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class ClickableHorizontalPanel extends HorizontalPanel implements HasClickHandlers {
+
+	@Override
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		return addDomHandler(handler, ClickEvent.getType());
+	}
+}
