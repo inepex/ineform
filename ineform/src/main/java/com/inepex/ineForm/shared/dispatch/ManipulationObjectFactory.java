bd4bd81820bd9be46d9b09e6d10fb8081adaa7a9
tibor.somodi <tibor.somodi@inepex.com>
2015-01-07 12:34:05 +0100

    remove customprop related classes

--- ineform/src/main/java/com/inepex/ineForm/shared/dispatch/ManipulationObjectFactory.java
+++ ineform/src/main/java/com/inepex/ineForm/shared/dispatch/ManipulationObjectFactory.java
@@ -1,6 +1,5 @@
 package com.inepex.ineForm.shared.dispatch;
 
-import com.inepex.ineom.shared.dispatch.interfaces.CustomObjectDescResultInterface;
 import com.inepex.ineom.shared.dispatch.interfaces.ObjectListResult;
 import com.inepex.ineom.shared.dispatch.interfaces.ObjectManipulationResult;
 import com.inepex.ineom.shared.dispatch.interfaces.RelationListResult;
