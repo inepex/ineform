601d63780ebab984e186e1745733a20d7ab9aeb5
balint.steinbach <balint.steinbach@inepex.com>
2015-01-14 11:13:54 +0100

    empty IneformAsyncCallback class

---
+++ ineframe/src/main/java/com/inepex/ineFrame/shared/EmptyIneformAsyncCallback.java
@@ -0,0 +1,10 @@
+package com.inepex.ineFrame.shared;
+
+public class EmptyIneformAsyncCallback implements IneformAsyncCallback<Void> {
+
+	@Override
+	public void onResponse(Void response) {
+		
+	}
+
+}
