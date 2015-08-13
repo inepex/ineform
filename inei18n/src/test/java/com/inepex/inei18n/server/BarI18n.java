8be1cf6e68e81af908f5a5fbc94ebcbb3d8a574d
tiborsomodi <tibor.somodi@inepex.com>
2015-08-13 16:08:00 +0200

    unformatted version of generated files

--- inei18n/src/test/java/com/inepex/inei18n/server/BarI18n.java
+++ inei18n/src/test/java/com/inepex/inei18n/server/BarI18n.java
@@ -5,43 +5,46 @@ import com.inepex.inei18n.shared.I18nModuleProvider;
 
 public class BarI18n extends I18nModule {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static final String MODULE_NAME = "BarI18n";
+	public static final String MODULE_NAME = "BarI18n";
+	
+	static I18nModuleProvider<BarI18n> moduleProvider;
+	
+	public BarI18n() {
+		super(MODULE_NAME);
+	}
+		
+	public BarI18n(I18nModuleProvider<BarI18n> moduleProvider) {
+		super(MODULE_NAME);
+		BarI18n.moduleProvider = moduleProvider;
+	}
+	
+	public static String getI18nText(String key){
+		return moduleProvider.get().getText(key);
+	}
 
-    static I18nModuleProvider<BarI18n> moduleProvider;
+	@Override
+	public I18nModuleProvider<?> getI18nProvider() {
+		return moduleProvider;
+	}
 
-    public BarI18n() {
-        super(MODULE_NAME);
-    }
-
-    public BarI18n(I18nModuleProvider<BarI18n> moduleProvider) {
-        super(MODULE_NAME);
-        BarI18n.moduleProvider = moduleProvider;
-    }
-
-    public static String getI18nText(String key) {
-        return moduleProvider.get().getText(key);
-    }
-
-    @Override
-    public I18nModuleProvider<?> getI18nProvider() {
-        return moduleProvider;
-    }
-
-    /**
-     * <u><i>Description:</i></u> Description for bar1 <br />
-     * <u><i>In English:</i></u> bar1_EN <u><i>Magyarul:</i></u> bar1_HU
-     */
-    public static String bar1() {
-        return moduleProvider.get().getText("bar1");
-    }
-
-    /**
-     * <u><i>Description:</i></u> Description for bar2 <br />
-     * <u><i>In English:</i></u> bar2_EN <u><i>Magyarul:</i></u> bar2_HU
-     */
-    public static String bar2() {
-        return moduleProvider.get().getText("bar2");
-    }
+	
+	/**
+	* <u><i>Description:</i></u> Description for bar1 <br />
+	* <u><i>In English:</i></u> bar1_EN
+	* <u><i>Magyarul:</i></u> bar1_HU
+	*/
+	public static String bar1() {
+		return moduleProvider.get().getText("bar1");
+	}
+	
+	/**
+	* <u><i>Description:</i></u> Description for bar2 <br />
+	* <u><i>In English:</i></u> bar2_EN
+	* <u><i>Magyarul:</i></u> bar2_HU
+	*/
+	public static String bar2() {
+		return moduleProvider.get().getText("bar2");
+	}
 }
