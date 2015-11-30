93edaf1864a118b6f90a89d7e1e3cb1df627c9f6
balint.steinbach <balint.steinbach@inepex.com>
2015-11-30 12:14:15 +0100

    Use string constant

--- ineform/src/main/java/com/inepex/ineForm/server/guice/IneFormServletModule.java
+++ ineform/src/main/java/com/inepex/ineForm/server/guice/IneFormServletModule.java
@@ -2,12 +2,13 @@ package com.inepex.ineForm.server.guice;
 
 import com.google.inject.servlet.ServletModule;
 import com.inepex.ineForm.server.csv.ExportServlet;
+import com.inepex.ineom.shared.IFConsts;
 
 public class IneFormServletModule extends ServletModule {
 
     @Override
     protected void configureServlets() {
-        serve("/export").with(ExportServlet.class);
+        serve(IFConsts.exportServletUrl).with(ExportServlet.class);
     }
 
 }
