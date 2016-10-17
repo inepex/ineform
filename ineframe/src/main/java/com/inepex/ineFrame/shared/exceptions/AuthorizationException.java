a4029505b90ddb251799d580f39cd911e0218321
SoTi <tibor.somodi@inepex.com>
2016-10-17 10:29:14 +0200

    AuthorizationException IsSerializable

--- ineframe/src/main/java/com/inepex/ineFrame/shared/exceptions/AuthorizationException.java
+++ ineframe/src/main/java/com/inepex/ineFrame/shared/exceptions/AuthorizationException.java
@@ -1,8 +1,10 @@
 package com.inepex.ineFrame.shared.exceptions;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 import net.customware.gwt.dispatch.shared.ActionException;
 
-public class AuthorizationException extends ActionException {
+public class AuthorizationException extends ActionException implements IsSerializable {
 
     private static final long serialVersionUID = 1L;
 
